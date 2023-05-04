34040da3336e7f289d9a121408ace98ba5aea4cc
kobaken <kentafly88@gmail.com>
2023-05-04 16:50:42 +0900

    minil new

---
+++ lib/Type/Simple.pm
@@ -0,0 +1,39 @@
+package Type::Simple;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Type::Simple - It's new $module
+
+=head1 SYNOPSIS
+
+    use Type::Simple;
+
+=head1 DESCRIPTION
+
+Type::Simple is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
